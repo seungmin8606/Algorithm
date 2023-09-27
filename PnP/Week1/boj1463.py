eb8d6a718eb7161597d9dd025eff201ad65f015a
Lee SeungMin <91493937+seungmin8606@users.noreply.github.com>
2023-09-27 11:14:44 +0900

    Update boj1463.py

--- PnP/Week1/boj1463.py
+++ PnP/Week1/boj1463.py
@@ -14,15 +14,13 @@ else:
     for i in range(4, N+1):
         if i % 6 == 0:
             a = min(count[i//3], count[i//2], count[i-1])
-            count[i] = a + 1
         elif i % 3 == 0:
             a = min(count[i//3], count[i-1])
-            count[i] = a + 1
         elif i % 2 == 0:
             a = min(count[i//2], count[i-1])
-            count[i] = a + 1
         else:
             a = count[i-1]
-            count[i] = a + 1
+            
+        count[i] = a + 1
 
 print(count[N])
