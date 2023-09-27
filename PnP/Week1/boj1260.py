8144df5503b213fabf27ad4341bf3844ca6be9ff
Lee SeungMin <91493937+seungmin8606@users.noreply.github.com>
2023-09-27 11:07:36 +0900

    Create boj1260.py

---
+++ PnP/Week1/boj1260.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+if N < 4:
+    count = [0, 0, 1, 1]
+else:
+    count = [0] * (N + 1)
+    count[2] = 1
+    count[3] = 1
+    for i in range(4, N+1):
+        if i % 6 == 0:
+            a = min(count[i//3], count[i//2], count[i-1])
+            count[i] = a + 1
+        elif i % 3 == 0:
+            a = min(count[i//3], count[i-1])
+            count[i] = a + 1
+        elif i % 2 == 0:
+            a = min(count[i//2], count[i-1])
+            count[i] = a + 1
+        else:
+            a = count[i-1]
+            count[i] = a + 1
+
+print(count[N])
